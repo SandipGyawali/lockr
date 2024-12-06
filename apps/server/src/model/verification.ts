e6400b08816bf501c817ec0578a7a7762c1761d5
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-12-06 12:12:19 +0545

    drizzle migration file fixes and update

--- apps/server/src/model/verification.ts
+++ apps/server/src/model/verification.ts
@@ -1,5 +1,14 @@
-import { pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 import { userSchema } from "./user";
+import { Verification } from "../common/enums/verification.enum";
+
+/**
+ * mapping typescript enum with
+ */
+export const verificationEnum = pgEnum(
+  "verification_enum",
+  Object.values(Verification) as [Verification, ...Verification[]],
+);
 
 export const verificationSchema = pgTable("t_verification", {
   id: uuid("id").primaryKey().defaultRandom().notNull(),
@@ -7,7 +16,7 @@ export const verificationSchema = pgTable("t_verification", {
     .references(() => userSchema.id)
     .notNull(),
   code: varchar("code", { length: 24 }),
-  type: text("type"),
+  type: verificationEnum("type"),
   createdAt: timestamp("created_at").defaultNow(),
   expiresAt: timestamp("expires_at").notNull(),
 });
